940c253d9331b847ce2c33cf4243ce830c6af1b3
Mindaugas <mkunevicius@gmail.com>
2016-07-04 17:44:12 +0300

    Redux-Promise added, building List Container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  render() {
+    return (
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+// ^ this in ES6 is the same as:
+// function mapStateToProps(state) {
+//   return { weather: state.weather };
+// }
+
+export default connect(mapStateToProps)(WeatherList);
